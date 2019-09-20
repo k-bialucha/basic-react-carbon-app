16438d4fe9eb3a0255f6b580cdef960b606ca64d
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-09-20 17:05:30 +0200

    ESLint/Webpack configuration adjusted

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
 
 const config = require('carbon-factory/webpack.config')({
-  parcelifyPaths: [
-    path.resolve(process.cwd(), 'node_modules/carbon-react')
-  ]
+  parcelifyPaths: [path.resolve(process.cwd(), 'node_modules/carbon-react')],
+  singlePageApp: true,
 });
 
 module.exports = config;
