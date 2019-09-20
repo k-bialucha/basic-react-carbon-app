fa409beb84123e61b4229e3911853d772625f903
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-09-20 17:13:05 +0200

    Basic routing configured; children support added to PageWrapper

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import AppWrapper from 'carbon-react/lib/components/app-wrapper/app-wrapper';
 import PageHeader from './PageHeader.jsx';
 
-const PageWrapper = () => {
+const PageWrapper = ({ children }) => {
   return (
     <div>
       <PageHeader />
-      <AppWrapper>AppWrapper content</AppWrapper>
+      <AppWrapper>{children}</AppWrapper>
     </div>
   );
+};
+
+PageWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default PageWrapper;
--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, Redirect } from 'react-router';
 import { startRouter } from 'carbon-react/lib/utils/router';
 
 // Loads base css from carbon:
 import 'carbon-react/lib/utils/css';
 
 import PageWrapper from './components/PageWrapper.jsx';
+import LandingPage from './views/pages/LandingPage.jsx';
+import FirstPage from './views/pages/FirstPage.jsx';
+import SecondPage from './views/pages/SecondPage.jsx';
 
 // Defines routes and corresponding components for app
 // (see ReactRouter for more information):
-startRouter(<Route path="/" component={PageWrapper} />);
+startRouter(
+  <Route component={PageWrapper}>
+    <Route path="/" component={LandingPage} />
+    <Route path="/first" component={FirstPage} />
+    <Route path="/second" component={SecondPage} />
+    <Redirect from="/*" to="/" />
+  </Route>
+);
 
 // Enables hot reloading through webpack:
 if (module.hot) {
