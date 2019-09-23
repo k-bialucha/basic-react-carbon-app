2197af487f7f4d7fc0a30ac313ddc0ea93f530e4
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-09-23 13:38:45 +0200

    Selected state handled for the navigation menu in the PageHeader component

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Link } from 'react-router';
 
 import NavigationBar from 'carbon-react/lib/components/navigation-bar/navigation-bar';
 import { Menu, MenuItem } from 'carbon-react/lib/components/menu/menu';
 
-const PageHeader = () => {
+const checkIfSelected = (path, routes) => {
+  const pathIndex = routes.findIndex(route => route.path === path);
+  const isSelected = pathIndex !== -1;
+
+  return isSelected;
+};
+
+const PageHeader = ({ routes }) => {
   return (
     <NavigationBar>
       <Menu>
         <Link to="/">
-          <MenuItem>Landing</MenuItem>
+          <MenuItem selected={checkIfSelected('/', routes)}>Landing</MenuItem>
         </Link>
         <Link to="/first">
-          <MenuItem>First Page</MenuItem>
+          <MenuItem selected={checkIfSelected('/first', routes)}>
+            First Page
+          </MenuItem>
         </Link>
         <Link to="/second">
-          <MenuItem>Second Page</MenuItem>
+          <MenuItem selected={checkIfSelected('/second', routes)}>
+            Second Page
+          </MenuItem>
         </Link>
       </Menu>
     </NavigationBar>
   );
+};
+
+PageHeader.propTypes = {
+  routes: PropTypes.array.isRequired,
 };
 
 export default PageHeader;
--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -4,16 +4,21 @@ import PropTypes from 'prop-types';
 import AppWrapper from 'carbon-react/lib/components/app-wrapper/app-wrapper';
 import PageHeader from './PageHeader.jsx';
 
-const PageWrapper = ({ children }) => {
+const PageWrapper = ({ router, children }) => {
+  const { routes } = router;
+
   return (
     <div>
-      <PageHeader />
+      <PageHeader routes={routes} />
       <AppWrapper>{children}</AppWrapper>
     </div>
   );
 };
 
 PageWrapper.propTypes = {
+  router: PropTypes.shape({
+    routes: PropTypes.array.isRequired,
+  }).isRequired,
   children: PropTypes.node.isRequired,
 };
 
