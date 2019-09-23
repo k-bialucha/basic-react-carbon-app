65976918a0ae21967d43feccc90e5203e7cbd97e
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-09-23 12:33:35 +0200

    Content components generation enhanced

--- src/views/pages/FirstPage/index.js
+++ src/views/pages/FirstPage/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 import Content from './Content.jsx';
 
+const borderColors = ['indianred', 'mediumorchid', 'teal'];
+const sampleTexts = [
+  'At vero eos et accusamus et iusto odio dignissimos ducimus quiblanditiis praesentium voluptatum deleniti atque corrupti quosdolores et quas molestias excepturi sint occaecati cupiditate nonprovident, similique sunt in culpa qui officia deserunt mollitiaanimi',
+  'Laborum et dolorum fuga. Et harum quidem rerum facilis',
+  'Est et expedita distinctio. Nam libero tempore, cum soluta nobis esteligendi optio cumque nihil impedit quo minus id quod maxime placeatfacere possimus, omnis voluptas assumenda est, omnis dolor',
+];
+
 const generateContentComponent = number => {
-  return () => <Content text={number} />;
+  const order = number % borderColors.length;
+  const borderColor = borderColors[order];
+  const text = sampleTexts[order];
+
+  return () => <Content text={text} borderColor={borderColor} />;
 };
 
 export const Content1 = generateContentComponent(1);
