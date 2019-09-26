465662a84d00e5c33591f3158677d4e6e8bea668
Kamil Bialucha <kamil.bialucha@gmail.com>
2019-09-26 12:01:51 +0200

    Theme support added to HeaderText component

--- src/components/HeaderText.jsx
+++ src/components/HeaderText.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import { Text } from './HeaderText.style';
 
+import ThemeContext from '../data/ThemeContext';
+
 const HeaderText = ({ text }) => {
-  return <Text>{text}</Text>;
+  const { variables } = useContext(ThemeContext);
+  return <Text theme={variables}>{text}</Text>;
 };
 
 HeaderText.propTypes = {
--- src/components/HeaderText.style.js
+++ src/components/HeaderText.style.js
@@ -1,17 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
 
-const blueSky = '#1573E6';
-
 export const Text = styled.div`
   text-align: center;
 
   font-size: 1.3em;
   font-weight: 600;
 
-  color: ${blueSky};
+  color: ${props => props.theme.primary};
 
   padding-right: 15px;
   margin-right: 10px;
-  border-right: 5px solid ${blueSky};
+  border-right: 5px solid ${props => props.theme.primary};
 `;
